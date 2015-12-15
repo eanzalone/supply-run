51de7375e3128ac7ef1661b1732ea992441cd780
eanzalone <emily.anzalone@gmail.com>
2015-12-15 15:16:36 -0800

    Heroku server

--- models/index.js
+++ models/index.js
@@ -1,4 +1,11 @@
 var mongoose = require('mongoose');
-mongoose.connect(process.env.PORT || 'mongodb://localhost/project3');
+// mongoose.connect(process.env.PORT || 'mongodb://localhost/project3');
+
+mongoose.connect(
+  process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/project3'
+);
+
 
 module.exports.Chat = require('./chat');
